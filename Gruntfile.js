fb297e2fea11c84305735828908f2c7eeb7f4d7d
Julián Gutiérrez <juliangut@gmail.com>
2015-11-18 23:23:53 +0100

    change phpmd rulesets

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     phpmd: {
       options: {
         bin: 'vendor/bin/phpmd',
-        rulesets: 'phpmd.xml',
+        rulesets: 'unusedcode,naming,design,controversial,codesize',
         reportFormat: 'text'
       },
       application: {
